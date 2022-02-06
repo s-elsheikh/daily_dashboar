ecd6962b87cdb09320b6bc34f4da77679c51e838
samer <samer.elsheikh@uniklinik-freiburg>
2022-02-06 17:48:08 +0100

    semi_initial commit

---
+++ daily_dsa_app/app.R
@@ -0,0 +1,63 @@
+library(shiny)
+library(tidyverse)
+library(readtext)
+library(lubridate)
+
+
+
+ui <- fluidPage(
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons("pats", "Todays_Patients", choices = c("")),
+            h5("click pat to see labor werte from 0:00 Uhr")
+        ),
+        mainPanel(
+            tabsetPanel(
+                # using iframe along with tags() within tab to display pdf with scroll, height and width could be adjusted
+                # tabPanel("Laborwerte vom gestern Abend", 
+                #          tags$iframe(style="height:600px; width:100%; scrolling=yes", 
+                #                      src=paste0(output$path, "/befs_pat_1.pdf")))
+                
+                
+                
+                tabPanel("Laborwerte vom gestern Abend",
+                         tags$iframe(style="height:600px; width:100%; scrolling=yes",
+                                     src="pdf_folder/befs_pat_1.pdf"))
+                
+                
+                ))
+        
+    
+       
+))
+
+server <- function(input, output, session) {
+    f <- stamp_date("2020_05_25.pdf")
+    f_name <- str_c("../pdfs/", f(today()-1))
+    tod_pdf <- readtext::readtext(f_name)
+    tex <- tod_pdf$text
+    df <- str_split(tex, "\\n")[[1]] %>% tibble(text = .)
+    todays_pats <- df %>% mutate(text = str_squish(text)) %>% 
+        filter(str_detect(text, "\\|\\(\\s\\*\\d")|
+                   lag(str_detect(text, "\\|\\(\\s\\*\\d"))) %>% 
+        transmute(
+            new = if_else(str_detect(text, "\\|\\(\\s\\*\\d"),
+                          str_c(text, lead(text)),
+                          "NA")) %>% 
+        filter (new != "NA") %>% 
+        mutate(
+            name = str_extract(new, ".+,\\s\\w+(?=\\|)") %>% str_remove_all(.,"\\d+") %>% str_squish(.),
+            dob = str_extract(new, "(?<=\\*)\\d{2}\\.\\d{2}\\.\\d{4}"),
+            piz = str_extract(new, "\\d{8}")
+        ) %>% select(-new)
+    names_vect <- todays_pats$name
+    updateRadioButtons(inputId = "pats", choices = names_vect)
+    
+    
+   addResourcePath("pdf_folder","H:/Abteilung Verwaltung/prepare_DSA/pdfs")
+    #output$path <- renderUI({tags$iframe(src="pdf_folder/1.pdf",style="height:800px; width:100%;scrolling=yes")})
+    
+  
+}
+
+shinyApp(ui, server)
---
+++ get_dsa_data_tomorrow.ahk
@@ -0,0 +1,55 @@
+SetKeyDelay, 1000
+
+get_tom_dsa()
+
+
+
+get_tom_dsa(st := 0, en := 0){
+	start_date := ""
+	end_date := ""
+	EnvAdd, start_date, %st%, Days 
+	EnvAdd, end_date, %en%, Days 
+	
+	mess := st "___" start_date "____" en "________" end_date
+	FormatTime, start_day, %start_date%, dd ; get single elements of start and end date 
+	FormatTime, start_month, %start_date%, MM
+	FormatTime, start_year, %start_date%, YYYY
+	FormatTime, end_day, %end_date%, dd
+	FormatTime, end_month, %end_date%, MM
+	FormatTime, end_year, %end_date%, YYYY
+	
+	FormatTime, save_start_date, %start_date%, yyyy_MM_dd
+	FormatTime, save_end_date, %end_date%, yyyy_MM_dd
+	
+	f_name := "H:\Abteilung Verwaltung\prepare_DSA\pdfs\" save_start_date ".pdf"
+	
+	
+	if FileExist(f_name)
+	{
+		FileDelete, %f_name%
+		
+	}
+	
+	
+	; MsgBox,,,test,1 ; unkown reason does not register key presses in termin planer
+	SetKeyDelay, 1000
+	WinActivate, Terminplan - Terminplanung
+	WinMaximize, Terminplan - Terminplanung
+	Sleep, 1500
+	
+	MouseClick, Left, 414, 25 ; clicks in termin planer to prepare for keystrokes
+	CoordMode, Mouse, Client
+	MouseClick, l, 457, 68 ; pick dsa
+	Sleep, 5000
+	MouseClick, l, 1132, 121 ; click today
+	Sleep, 5000
+	Send, ^p
+	Send, `n
+	WinWait, Druckausgabe speichern unter
+	Sleep, 300
+	SetKeyDelay, 100
+	Send, %f_name%
+	SetKeyDelay, 1000
+	Send, `n
+	
+}
